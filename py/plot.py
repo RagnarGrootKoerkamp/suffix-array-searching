5ef4376ec43fdb0aef0acbe25e44fc3f5673a4d3
František Kmječ <fkmjec@vmi526354.contaboserver.net>
2024-10-27 17:46:43 +0100

    Add line names and legend

--- py/plot.py
+++ py/plot.py
@@ -3,21 +3,22 @@ import matplotlib.pyplot as plt
 
 # placeholder for now to see some results
 START_POW2 = 10
-STOP_POW2 = 28
+STOP_POW2 = 20
 NUM_REPEATS = 50
 to_try = ["basic_binsearch", "basic_binsearch_branchless"]
 
-def plot_results(sizes, timings, comparisons, name="plot.pdf"):
+def plot_results(sizes, names, timings, comparisons, filename="plot.pdf"):
     fig, ax = plt.subplots()
-    for timing in timings:
-        ax.plot(sizes, timing)
+    for name, timing in zip(names, timings):
+        ax.plot(sizes, timing, label=name)
     # Customize the plot
     ax.set_title("Performance plot")
     ax.set_xlabel("size of inputs")
     ax.set_ylabel("time in ns")
     ax.set_xscale('log', base=2)
+    ax.legend()
     ax.grid(True)               # Show grid for readability
-    fig.savefig(name, bbox_inches="tight")
+    fig.savefig(filename, bbox_inches="tight")
 
 b = sa_layout.BenchmarkSortedArray(2**STOP_POW2)
 sizes = [2**x for x in range(START_POW2, STOP_POW2)]
@@ -28,4 +29,4 @@ for name in to_try:
     timings.append(timing)
     comparisons.append(comparison)
 
-plot_results(sizes, timings, comparisons, name="basic_binsearch")
+plot_results(sizes, to_try, timings, comparisons, filename="plot.png")
