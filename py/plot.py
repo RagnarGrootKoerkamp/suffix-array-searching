dd884750676099ec78381fb94bf334e04f6e2ed7
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2024-11-11 14:19:13 +0100

    update plot.py

--- py/plot.py
+++ py/plot.py
@@ -1,11 +1,13 @@
+#!/usr/bin/python
 import sa_layout
 import matplotlib.pyplot as plt
 
 # placeholder for now to see some results
-START_POW2 = 10
-STOP_POW2 = 20
-NUM_REPEATS = 50
+START_POW2 = 20
+STOP_POW2 = 26
+NUM_REPEATS = 1000000
 to_try = ["basic_binsearch", "basic_binsearch_branchless"]
+
 
 def plot_results(sizes, names, timings, comparisons, filename="plot.pdf"):
     fig, ax = plt.subplots()
@@ -15,15 +17,17 @@ def plot_results(sizes, names, timings, comparisons, filename="plot.pdf"):
     ax.set_title("Performance plot")
     ax.set_xlabel("size of inputs")
     ax.set_ylabel("time in ns")
-    ax.set_xscale('log', base=2)
+    ax.set_xscale("log", base=2)
     ax.legend()
-    ax.grid(True)               # Show grid for readability
+    ax.grid(True)  # Show grid for readability
     fig.savefig(filename, bbox_inches="tight")
+
 
 b = sa_layout.BenchmarkSortedArray(2**STOP_POW2)
 sizes = [2**x for x in range(START_POW2, STOP_POW2)]
 timings = []
 comparisons = []
+print("START BENCHING")
 for name in to_try:
     timing, comparison = b.benchmark(name, START_POW2, STOP_POW2, NUM_REPEATS)
     timings.append(timing)
