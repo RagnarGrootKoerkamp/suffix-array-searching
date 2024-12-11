dde29be5ef317b2e636a5c7c03c7899af2f8bf61
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2024-12-11 22:59:41 +0100

    Add 'Reverse' B+tree that stores max of left child insteaf of min of right child

--- src/bplustree.rs
+++ src/bplustree.rs
@@ -10,17 +10,20 @@ use crate::{
 // N total elements in a node.
 // B branching factor.
 // B-1 actual elements in a node.
+// REV: when true, nodes contain the largest elements of their first B (of B+1) children,
+//      rather than the smallest elements of the last B children.
 #[derive(Debug)]
-pub struct BpTree<const B: usize, const N: usize> {
+pub struct BpTree<const B: usize, const N: usize, const REV: bool> {
     tree: Vec<BTreeNode<B, N>>,
     pub cnt: usize,
     offsets: Vec<usize>,
 }
 
-pub type BpTree16 = BpTree<16, 16>;
-pub type BpTree15 = BpTree<15, 16>;
+pub type BpTree16 = BpTree<16, 16, false>;
+pub type BpTree15 = BpTree<15, 16, false>;
+pub type BpTree16R = BpTree<16, 16, true>;
 
-impl<const B: usize, const N: usize> BpTree<B, N> {
+impl<const B: usize, const N: usize, const REV: bool> BpTree<B, N, REV> {
     fn blocks(n: usize) -> usize {
         n.div_ceil(B)
     }
@@ -67,16 +70,25 @@ impl<const B: usize, const N: usize> BpTree<B, N> {
             for i in 0..B * (bptree.offsets[h + 1] - bptree.offsets[h]) {
                 let mut k = i / B;
                 let j = i % B;
-                k = k * (B + 1) + j + 1; // compare to right of key
-                                         // and then to the left
+                k = k * (B + 1) + j + 1;
+                // compare to right of key
+                // and then to the left
                 for _l in 0..h - 1 {
                     k *= B + 1;
                 }
-                bptree.tree[bptree.offsets[h] + i / B].data[i % B] = if k * B < n {
-                    bptree.tree[k].data[0]
+                if !REV {
+                    bptree.tree[bptree.offsets[h] + i / B].data[i % B] = if k * B < n {
+                        bptree.tree[k].data[0]
+                    } else {
+                        MAX
+                    };
                 } else {
-                    MAX
-                };
+                    bptree.tree[bptree.offsets[h] + i / B].data[i % B] = if k * B < n {
+                        bptree.tree[k - 1].data[B - 1]
+                    } else {
+                        MAX
+                    };
+                }
             }
         }
         bptree
@@ -91,6 +103,7 @@ impl<const B: usize, const N: usize> BpTree<B, N> {
     }
 
     pub fn search(&mut self, q: u32) -> u32 {
+        assert!(!REV);
         let mut k = 0;
         for o in self.offsets[1..self.offsets.len() - 1].into_iter().rev() {
             let jump_to = self.node(o + k).find(q);
@@ -102,6 +115,7 @@ impl<const B: usize, const N: usize> BpTree<B, N> {
     }
 
     pub fn search_split(&mut self, q: u32) -> u32 {
+        assert!(!REV);
         let mut k = 0;
         for o in self.offsets[1..self.offsets.len() - 1].into_iter().rev() {
             let jump_to = self.node(o + k).find_split(q);
@@ -113,6 +127,7 @@ impl<const B: usize, const N: usize> BpTree<B, N> {
     }
 
     pub fn batch<const P: usize>(&mut self, q: &[u32; P]) -> [u32; P] {
+        assert!(!REV);
         let mut k = [0; P];
         for o in self.offsets[1..self.offsets.len() - 1].into_iter().rev() {
             for i in 0..P {
@@ -128,6 +143,7 @@ impl<const B: usize, const N: usize> BpTree<B, N> {
     }
 
     pub fn batch_prefetch<const P: usize>(&mut self, q: &[u32; P]) -> [u32; P] {
+        assert!(!REV);
         let mut k = [0; P];
         let q_simd = q.map(|q| Simd::<u32, 8>::splat(q));
         for h in (1..self.offsets.len() - 1).rev() {
@@ -147,6 +163,7 @@ impl<const B: usize, const N: usize> BpTree<B, N> {
     }
 
     pub fn batch_ptr<const P: usize>(&mut self, q: &[u32; P]) -> [u32; P] {
+        assert!(!REV);
         let mut k = [0; P];
         let q_simd = q.map(|q| Simd::<u32, 8>::splat(q));
 
@@ -173,6 +190,7 @@ impl<const B: usize, const N: usize> BpTree<B, N> {
     }
 
     pub fn batch_ptr2<const P: usize>(&mut self, q: &[u32; P]) -> [u32; P] {
+        assert!(!REV);
         let mut k = [0; P];
         let q_simd = q.map(|q| Simd::<u32, 8>::splat(q));
 
@@ -195,7 +213,7 @@ impl<const B: usize, const N: usize> BpTree<B, N> {
         let o = unsafe { *offsets.get_unchecked(0) };
         std::array::from_fn(|i| {
             let mut index = unsafe { *o.byte_add(k[i]) }.find_splat(q_simd[i]);
-            if index == B {
+            if !REV && index == B {
                 index = N;
             }
             unsafe { (o.byte_add(k[i]) as *const u32).add(index).read() }
@@ -225,7 +243,7 @@ impl<const B: usize, const N: usize> BpTree<B, N> {
         let o = unsafe { *offsets.get_unchecked(0) };
         std::array::from_fn(|i| {
             let mut index = unsafe { *o.byte_add(k[i]) }.find_splat(q_simd[i]);
-            if index == B {
+            if !REV && index == B {
                 index = N;
             }
             unsafe { (o.byte_add(k[i]) as *const u32).add(index).read() }
@@ -241,7 +259,7 @@ mod tests {
     #[test]
     fn test_b_tree_k_2() {
         let vals = vec![1, 2, 3, 4, 5, 6, 7, 8];
-        let bptree = BpTree::<2, 2>::new(vals);
+        let bptree = BpTree::<2, 2, false>::new(vals);
         println!("{:?}", bptree);
     }
 
@@ -249,7 +267,7 @@ mod tests {
     fn test_b_tree_k_3() {
         let vals = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
         // let correct_output = vec![4, 8, 12, 1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15];
-        let computed_out = BpTree::<3, 3>::new(vals);
+        let computed_out = BpTree::<3, 3, false>::new(vals);
         println!("{:?}", computed_out);
     }
 
@@ -258,7 +276,7 @@ mod tests {
         let mut vals: Vec<u32> = (1..2000).collect();
         vals.push(MAX);
         let q = 452;
-        let mut bptree = BpTree::<16, 16>::new(vals.clone());
+        let mut bptree = BpTree::<16, 16, false>::new(vals.clone());
         let bptree_res = bptree.search(q);
 
         let binsearch_res = BinarySearch::new(vals).search(q);
@@ -271,7 +289,7 @@ mod tests {
         let mut vals: Vec<u32> = (1..2000).collect();
         vals.push(MAX);
         let q = 289;
-        let mut bptree = BpTree::<16, 16>::new(vals.clone());
+        let mut bptree = BpTree::<16, 16, false>::new(vals.clone());
         let bptree_res = bptree.search(q);
 
         let binsearch_res = BinarySearch::new(vals).search(q);
@@ -283,7 +301,7 @@ mod tests {
     fn test_simd_cmp() {
         let mut vals: Vec<u32> = (1..16).collect();
         vals.push(MAX);
-        let bptree = BpTree::<16, 16>::new(vals);
+        let bptree = BpTree::<16, 16, false>::new(vals);
         let idx = bptree.tree[0].find(1);
         println!("{}", idx);
         assert_eq!(idx, 0);
