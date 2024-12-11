30489e1483dec792e62af42b033ddcfcc0a534d3
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2024-12-11 21:25:09 +0100

    util: prefetch_ptr

--- src/util.rs
+++ src/util.rs
@@ -76,6 +76,11 @@ pub trait Search<'t> {
 /// Prefetch the given cacheline into L1 cache.
 pub fn prefetch_index<T>(s: &[T], index: usize) {
     let ptr = unsafe { s.as_ptr().add(index) as *const u64 };
+    prefetch_ptr(ptr);
+}
+
+/// Prefetch the given cacheline into L1 cache.
+pub fn prefetch_ptr<T>(ptr: *const T) {
     #[cfg(target_arch = "x86_64")]
     unsafe {
         std::arch::x86_64::_mm_prefetch(ptr as *const i8, std::arch::x86_64::_MM_HINT_T0);
