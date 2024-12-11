1db75a75efa116de8ac82403ed7084f552175c5a
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2024-12-11 21:19:38 +0100

    update rank_curve example

--- examples/rank_curve.rs
+++ examples/rank_curve.rs
@@ -12,15 +12,42 @@ fn main() {
     let args = Args::parse();
     let k = args.k;
 
-    let mut counts = vec![0; 1 << (2 * k)];
-    for i in 0..seq.len() - k + 1 {
-        let mut key = 0;
-        for j in 0..k {
-            key = key << 2 | seq[i + j] as usize;
-        }
+    let mut counts = vec![0u32; 1 << (2 * k)];
+    let mut key = 0;
+    for i in 0..k - 1 {
+        key = key << 2 | seq[i] as usize;
+    }
+    for i in k - 1..seq.len() {
+        key = key << 2 | seq[i] as usize;
+        key &= (1 << (2 * k)) - 1;
         counts[key] += 1;
     }
 
-    let out = serde_json::to_string(&counts).unwrap();
-    eprintln!("{}", out);
+    // histogram of sizes
+    let mut hist = vec![0; 30];
+    let mut sum = vec![0; 30];
+    for &c in &counts {
+        if c > 0 {
+            let x = c.ilog2() as usize;
+            hist[x] += 1;
+            sum[x] += c;
+        }
+    }
+
+    let kmers = seq.len() - k + 1;
+    let mut acc = 0;
+
+    for i in 0..hist.len() {
+        let h = hist[i];
+        let s = sum[i];
+        acc += s;
+        eprintln!(
+            "2^{i:>2} {h:>10} {s:>10} {acc:>10} {:>10.4} {:>10.4}",
+            acc as f64 / kmers as f64,
+            1. - acc as f64 / kmers as f64
+        );
+    }
+
+    // let out = serde_json::to_string(&counts).unwrap();
+    // eprintln!("{}", out);
 }
