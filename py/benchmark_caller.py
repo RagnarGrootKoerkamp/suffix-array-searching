0942099fceb2b006a4f29bb86ce9bd6320fabf37
Frantisek Kmjec <frantisek.kmjec@gmail.com>
2024-12-13 15:18:17 +0100

    Add a python script to call benchmarks

---
+++ py/benchmark_caller.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+import shlex
+
+def parse_output(cmd_output: str) -> list[tuple[int, float]]:
+    print(cmd_output)
+    lines = [line.split() for line in cmd_output.split("\n")]
+    lines = list(filter(lambda line: len(line) == 2, lines))
+    return [(int(arr[0]), float(arr[1])) for arr in lines]
+
+def get_measurements(fname: str, start_pow2: int, stop_pow2: int, queries: int) -> list[tuple[int, float]]:
+    project_path = os.path.join(os.path.dirname(__file__), "../")
+    args = shlex.split(f"cargo run --bin bench_one -- --fname {fname} --start {start_pow2} --stop {stop_pow2} --queries {queries}")
+    result = subprocess.check_output(args, cwd=project_path).decode()
+    return parse_output(result)
+
+if __name__ == "__main__":
+    print(get_measurements("bs_search", 10, 20, 1000))
